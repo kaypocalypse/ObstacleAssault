6f8eb8dc2c3842a94fce41a12a3b4c2acdb81648
kaypocalypse <74224857+kaypocalypse@users.noreply.github.com>
2023-07-12 12:18:16 -0700

    S3 44-47
    Created new C++ Class: MovingPlatform
    
    Created test variables and a calculator in beginplay.

---
+++ ObstacleAssault/Intermediate/Build/Win64/ObstacleAssaultEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.33.31630
+#include "E:/Content/Unreal Projects/UE 5 Cpp Udemy/ObstacleAssault/ObstacleAssault/ObstacleAssault/Intermediate/Build/Win64/ObstacleAssaultEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
